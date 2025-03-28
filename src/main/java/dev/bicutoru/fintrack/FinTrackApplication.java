498b27c49855e4e0bd8d9dea0c91fa81e3fe37c3
victorashino <victor.ashino.main@gmail.com>
2025-03-28 01:04:05 -0300

    first commit

---
+++ src/main/java/dev/bicutoru/fintrack/FinTrackApplication.java
@@ -0,0 +1,13 @@
+package dev.bicutoru.fintrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinTrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bicutoru/fintrack/FinTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bicutoru.fintrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
